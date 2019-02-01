91fdd968910df739b8a7aa822a675c7183b36156
Vinicius Ribeiro <vinicius.ribeiro@saphyr.com.br>
2019-02-01 19:23:44 -0200

    sends message to telegram channel

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const Watcher = require(`rss-watcher`);
 require('dotenv').config();
 
 const channel = `@techmeme`;
-// const feed = `https://www.techmeme.com/feed.xml`;
-const feed = `http://lorem-rss.herokuapp.com/feed?unit=second`;
+const feed = `https://www.techmeme.com/feed.xml`;
+// const feed = `http://lorem-rss.herokuapp.com/feed?unit=second&interval=5`;
 
 const api = new telegram({
     token: process.env.BOT_TOKEN,
@@ -13,30 +13,22 @@ const api = new telegram({
     }
 });
 
-// api.sendMessage({
-//     chat_id: channel,
-//     text: `Meu primeiro teste`
-// })
-
-// api
-//     .on(`message`, (message) => {
-//         api.sendMessage({
-//             chat_id: channel,
-//             text: message.text ? message.text : `Sem texto...`
-//         })
-//     });
-
 
 const watcher = new Watcher(feed);
 watcher.set({
-    interval: 10
+    interval: 5,
+    feed: feed
 })
-watcher.on(`new article`, (article) => {
-    console.log(article);
-    api.sendMessage({
-        chat_id: channel,
-        text: `Atualizou!`
-    });
+
+watcher
+    .on(`new article`, (entries) => {
+        console.log(entries);
+        api.sendMessage({
+            chat_id: channel,
+            text: `${entries.title}: ${entries.link}`
+        });
 });
+
+watcher.run();
 
 console.log(`esperando...`);
